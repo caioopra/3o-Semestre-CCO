f2ea9ea23b8df3188a3651044e89f9a5bf5ece6a
caioopra <caioprsilva@gmail.com>
2022-10-06 23:06:42 -0300

    PC: AF-Mutex finalizada

--- PC/unidade2/mutex/AF-mutex/AF-mutex/exercicio_3/main.c
+++ PC/unidade2/mutex/AF-mutex/AF-mutex/exercicio_3/main.c
@@ -1,38 +1,43 @@
+#include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-int gValue = 0;
 pthread_mutex_t gMtx;
 
 // Função imprime resultados na correção do exercício -- definida em helper.c
 void imprimir_resultados(int n, int** results);
 
 // Função escrita por um engenheiro
-void compute(int arg) {
-    if (arg < 2) {
-        pthread_mutex_lock(&gMtx);
-        gValue += arg;
-        pthread_mutex_unlock(&gMtx);
-    } else {
-        compute(arg - 1);
-        compute(arg - 2);
+int compute(int arg) {
+    if (arg > 0 && arg <= 2) {
+        return 1;
     }
+
+    int soma = 1;
+    int a = 1;
+    int b = 1;
+    int c = 0;
+
+    for (int i = 0; i < arg - 2; i++) {
+        c = a + b;
+        soma += c;
+
+        a = b;
+        b = c;
+    }
+
+    return soma;
 }
 
-// Função wrapper que pode ser usada com pthread_create() para criar uma 
+// Função wrapper que pode ser usada com pthread_create() para criar uma
 // thread que retorna o resultado de compute(arg
 void* compute_thread(void* arg) {
     int* ret = malloc(sizeof(int));
-    //pthread_mutex_lock(&gMtx);
-    gValue = 0;
-    compute(*((int*)arg));
+    int gValue = compute(*((int*)arg));
     *ret = gValue;
-    //pthread_mutex_unlock(&gMtx);
     return ret;
 }
-
 
 int main(int argc, char** argv) {
     // Temos n_threads?
@@ -42,36 +47,31 @@ int main(int argc, char** argv) {
     }
     // n_threads > 0 e foi dado um x para cada thread?
     int n_threads = atoi(argv[1]);
-    if (!n_threads || argc < 2+n_threads) {
+    if (!n_threads || argc < 2 + n_threads) {
         printf("Uso: %s n_threads x1 x2 ... xn\n", argv[0]);
         return 1;
     }
 
-    //Inicializa o mutex
-    pthread_mutex_init(&gMtx, NULL);
-
     int args[n_threads];
     int* results[n_threads];
     pthread_t threads[n_threads];
-    //Cria threads repassando argv[] correspondente
-    for (int i = 0; i < n_threads; ++i)  {
-        args[i] = atoi(argv[2+i]);
+
+    // Cria threads repassando argv[] correspondente
+    for (int i = 0; i < n_threads; ++i) {
+        args[i] = atoi(argv[2 + i]);
         pthread_create(&threads[i], NULL, compute_thread, &args[i]);
     }
     // Faz join em todas as threads e salva resultados
     for (int i = 0; i < n_threads; ++i)
         pthread_join(threads[i], (void**)&results[i]);
 
-    // Não usaremos mais o mutex
-    pthread_mutex_destroy(&gMtx);
-
     // Imprime resultados na tela
     // Importante: deve ser chamada para que a correção funcione
     imprimir_resultados(n_threads, results);
 
     // Faz o free para os resultados criados nas threads
-    for (int i = 0; i < n_threads; ++i) 
+    for (int i = 0; i < n_threads; ++i)
         free(results[i]);
-    
+
     return 0;
 }
