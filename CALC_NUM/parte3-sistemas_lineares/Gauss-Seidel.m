4f2cf2e72e90d8b3c4f9de7876a3c624d4b9d6fa
Caio Pra <caioprsilva@gmail.com>
2022-11-04 10:48:09 -0300

    PC: inicio AF openMP; C_NUM: sistemas lineares com Seidel e Jacobi

---
+++ CALC_NUM/parte3-sistemas_lineares/Gauss-Seidel.m
@@ -0,0 +1,35 @@
+clc
+clear
+
+# sistema de entrada
+a = [3 -1 -1; 1 3 1; 2 -2 4];
+b = [1 5 4];
+n = length(b);
+
+# estimativa inicial
+x0 = zeros(1, n);  # vetor de 1 linha n colunas de 0s
+xa = x0;    # x anterior; salva para calcular d
+
+erro = 10^-7;  # precisao
+d = 1;  # valor qualquer maior que erro
+
+k = 0;   # numero de iterações
+while (d > erro)
+    k += 1;
+    for i = 1 : n
+        soma = 0;
+        for j = 1 : n
+            if j ~= i
+                soma += a(i, j) * x0(j);
+            endif
+        endfor
+        x0(i) = (b(i) - soma) / a(i, i);
+    endfor
+    # depois de terminado todas as linhas
+    d = sum(abs(x0-xa));  # poder usar norma1(sum) ou max
+    xa = x0;
+endwhile
+
+k
+d
+x0
---
+++ CALC_NUM/parte3-sistemas_lineares/gauss-Jacobi.m
@@ -0,0 +1,37 @@
+clc
+clear
+
+# sistema de entrada
+a = [3 -1 -1; 1 3 1; 2 -2 4];
+b = [1 5 4];
+n = length(b);
+
+# estimativa inicial
+x0 = zeros(1, n)  # vetor de 1 linha n colunas de 0s
+
+erro = 10^-7;  # precisao
+d = 1;  # valor qualquer maior que erro
+
+k = 0;   # numero de iterações
+while (d > erro)
+    k += 1;
+
+    for i = 1 : n
+        soma = 0;
+        for j = 1 : n
+            if j ~= i
+                soma += a(i, j) * x0(j);
+            endif
+        endfor
+
+        x(i) = (b(i) - soma) / a(i, i);
+    endfor
+    # depois de terminado todas as linhas
+    d = sum(abs(x-x0));   # poder usar norma1(sum) ou max
+    x0 = x;
+endwhile
+
+k
+x
+d
+
